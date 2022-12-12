76b2bedc112e65870a52d68f3ddb361ca543902f
odevepdro <pedrosschmidt2@gmail.com>
2022-12-12 14:18:02 -0300

    servidor local e nodemona

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require("http");
 const port = 3000;
 
+
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros':'Entrei na pagina de livros',
+    '/autores':'Listagem de autores',
+    '/test': 'test',
+    '/another': 'hi'
+}
 //servidor
 const server = http.createServer((req, res) =>{
     res.writeHead(200, {'Content-Type': "text/plain"});
-    res.end('Curso de Node')
+    res.end(rotas[req.url]);
 })
 
 
